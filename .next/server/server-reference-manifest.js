b502176b93178ca059cf7027dade1a6e57941ca0
jithinsv <jvargh7@emory.edu>
2025-12-29 09:47:32 -0500

    Minor edits to description

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"lMuItDLsf3nP9rNCXQE5hXO+TxNy71QKw/P8ucUYjpc=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"B2BK/XayFxac0gQ4U4j7HxxP02gIPIUR29AU3g39KCA=\"\n}"
